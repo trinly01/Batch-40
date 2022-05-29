b4430793682098729e0165fba769b96cccf9110c
Trinmar Boado <timi.boado@gmail.com>
2022-05-29 14:51:24 +0800

    custom Events

--- frontend/src/components/SumComponent.vue
+++ frontend/src/components/SumComponent.vue
@@ -3,13 +3,18 @@
     {{ num1 }} + {{ num2 }} {{ data.val<1 ? '-' : '+' }} {{ Math.abs(data.val) }} = {{ answer }}
     <button @click="data.val++">increase</button>
     <button @click="data.val--">decrease</button>
+    <!-- <q-input v-model="data.message" /> -->
+    <button @click="$emit('hello', answer)">Emit check console</button>
   </div>
 </template>
 
 <script setup>
 import { computed, reactive } from 'vue'
 
+const $emit = defineEmits(['hello', 'update:modelValue'])
+
 const data = reactive({
+  message: '',
   val: 0
 })
 
@@ -25,7 +30,9 @@ const props = defineProps({
 })
 
 const answer = computed(() => {
-  return props.num1 + props.num2 + data.val
+  let answer = ''
+  $emit('update:modelValue', answer = props.num1 + props.num2 + data.val)
+  return answer
 })
 
 </script>
--- frontend/src/pages/trinTodo.vue
+++ frontend/src/pages/trinTodo.vue
@@ -12,20 +12,24 @@
     </q-toolbar>
     <q-input @keyup.enter="add" rounded standout v-model="data.newTodo" label="Rounded standout" />
     <q-list bordered separator>
-      <q-item clickable v-for="(todo, ind) in todos" :key="todo._id">
+      <q-item clickable @mouseleave="data.hovering = -1" @mouseover="data.hovering = ind" v-for="(todo, ind) in todos" :key="todo._id">
         <q-item-section avatar>
           <q-checkbox v-model="todo.isDone"></q-checkbox>
         </q-item-section>
-        <q-item-section>
+        <q-item-section :style="{
+          'text-decoration': todo.isDone ? 'line-through' : '',
+          'color':  todo.isDone ? 'gray' : ''
+        }">
           {{ ind }} {{ todo.label }}
         </q-item-section>
-        <q-item-section side>
+        <q-item-section side v-if="data.hovering === ind">
           <q-btn @click="remove(ind)" round icon="delete" color="red" size="sm" />
         </q-item-section>
       </q-item>
     </q-list>
     <pie-chart :donut="true" :data="[['Active', todos.length - completed], ['Completed', completed]]"></pie-chart>
-    <sum-component :num1="2" :num2="3" />
+    <h1>{{ data.initialValue }}</h1>
+    <sum-component v-model="data.initialValue" @hello="checkMessage" :num1="2" :num2="3" />
   </div>
 </template>
 
@@ -34,8 +38,14 @@ import SumComponent from 'src/components/SumComponent.vue'
 import { ref, reactive, computed } from 'vue'
 
 const data = reactive({
+  initialValue: 0,
+  hovering: -1,
   newTodo: ''
 })
+
+const checkMessage = (msg) => {
+  console.log('pogi', msg)
+}
 
 // const newTodo = ref('')
 const todos = ref([
